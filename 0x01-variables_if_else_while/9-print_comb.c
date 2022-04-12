4e7c80a7f610e792e941774e5b16525349bace0f
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2022-04-12 08:34:45 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	int n;
+		for (n = 48; n < 58; n++)
+			{
+			putchar(n);
+
+				if (n != 57)
+				{
+                                        putchar(',');
+					putchar(' ');
+				}
+			}
+				putchar('\n');
+					return (0);
+}
