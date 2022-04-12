871657a1fb0888b6f43b7947b63cd5c62d129961
airkongo <rodneymutembei1@gmail.com>
2022-04-11 21:13:50 -0700

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-int main(void)
-
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main (void)
 {
-
-			puts("\"Programming is like building a multilingual puzzle");
-
-						return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
