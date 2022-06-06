5d6aec90efb83a1a549c7b0af212aeba461b57f3
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2022-06-06 07:01:59 +0300

    _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char c;
+	c [8] = "_putchar";
+	int a = 0
+	while (a < 8)
+	{
+	_putchar(c[a]);
+	a++
+	}
+	_putchar('\ln');
+	return (0);
+}
